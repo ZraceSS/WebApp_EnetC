c15362891101abbb5884c890975cdc556812b8bb
ZraceSS <reprintx@gmail.com>
2024-11-05 08:53:38 +0700

    Fix delete

--- delete.php
+++ delete.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    if (isset($_SESSION['id']) && $_SESSION['role'] == 'a')
+    if (isset($_SESSION['id']))
     {
         $id = $_GET['id'];
         $conn=new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root", "");
